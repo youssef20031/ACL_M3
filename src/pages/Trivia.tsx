e84f92ad78c88ff20ae8a65ba24d141adc915577
seif495 <seifmoh495@gmail.com>
2026-06-01 17:54:22 +0300

    Small tweak to Fantasy Trivia on Mobile

--- src/pages/Trivia.tsx
+++ src/pages/Trivia.tsx
@@ -363,7 +363,7 @@ export function Trivia() {
         </AnimatePresence>
 
         {currentQuestion && (
-          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-4">
+          <div className="grid grid-cols-2 gap-3 sm:gap-4">
             <ScoreCard
               label="Score"
               value={`${triviaScore} / ${triviaTotal}`}
